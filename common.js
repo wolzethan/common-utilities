d2f704599775de6dfb4e8748adaf291ab45627a7
Ethan <ethan@eyeballdivision.com>
2017-09-03 09:57:19 -0600

    Initial commit

---
+++ common.js
@@ -0,0 +1,89 @@
+/**
+ * Common Utilities
+ * Author: Ethan Wolz <ethan@eyeballdivision.com>
+ */
+
+/**
+ * Deep Find
+ * @param {object} obj => Object to find from. 
+ * @param {prop} prop => Property to find.
+ */
+
+const deepFind = (obj, val) => {
+    for (let p in obj) {
+        if (p === val && obj[p]) return obj[p];
+
+        if (typeof obj[p] === 'object') {
+            let current = deepFind(obj[p], val);
+            if (current) return current;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * 
+ * @param {object} obj - Object to change
+ * @param {string} val - Key on object to set
+ * @param {*} set - Value to set val to 
+ * Returns new object with val set
+ */
+
+const deepSet = (obj, val, set) => {
+    for (let p in obj) {
+        if (p === val) {
+            obj[p] = set
+            return obj;
+        }
+
+        if (typeof obj[p] === 'object') {
+            let current = deepSet(obj[p], val, set);
+            if (current) return current;
+        }
+    }
+
+    return obj;
+}
+
+/**
+ * arrayFind => Search an array of objects for a non-null value specified by value
+ * @param {Array} arr => Array you will be searching
+ * @param {string} value => the key you want to search by
+ */
+
+const arrayFind = (arr) => (value) => {
+    if (typeof value !== 'string') throw new Error('Invalid datatype');
+
+    for (let i = 0; i < arr.length; i++) {
+        let current = deepFind(arr[i], value);
+        if (current) return current;
+    }
+}
+
+const sortBy = arr => (value, push) => {
+    return arr.reduce((sum, val) => {
+        let v = deepFind(val, value);
+        if (sum[v]) {
+            sum[v][push].push(val);
+        } else {
+            sum[v] = {};
+            sum[v][push] = [val];
+        }
+        return sum;
+    }, {});
+}
+
+const arraySet = (arr) => (prop, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        deepSet(arr[i], prop, value);
+    }
+
+    return arr;
+}
+
+exports.deepFind = deepFind;
+exports.deepSet  = deepSet;
+exports.arrayFind = arrayFind;
+exports.sortBy    = sortBy;
+exports.arraySet  = arraySet;
---
+++ test/test-common.js
@@ -0,0 +1,163 @@
+const _ = require('lodash');
+
+const { 
+    deepFind, 
+    combine, 
+    deepSet, 
+    arrayFind,
+    sortBy,
+    compare,
+    arraySet,
+    findTid,
+    setTid,
+ } = require('../common');
+
+let assert = require('assert');
+
+describe('deepSet:', () => {
+    const a = {
+        b: {
+            c: {
+                'test': 'This should not exist'
+            }
+        },
+        c: {
+            'test': 'this should not exist'
+        }
+    };
+
+    const set = ['Working'];
+
+    it('should set all values deep into object', () => {
+        deepSet(a, 'c', set);
+        assert.equal(a.b.c, set);
+    });
+});
+
+describe('deepFind:', () => {
+    const deepNested = {
+        a: {
+            b: {
+                c: {
+                    d: {
+                        e: {
+                            f: 5
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    it('should return the value 5 from the deep nested object', () => {
+        assert.equal(5, deepFind(deepNested, 'f'))
+    });
+});
+
+describe('arrayFind:', () => {
+    const arr = [
+        {
+            x: {
+                y: null
+            }
+        },
+        {
+            x: {
+                y: null
+            }
+        },
+        {
+            x: {
+                y: null
+            }
+        },
+        {
+            x: {
+                y: null
+            }
+        },
+        {
+            x: {
+                y: 50
+            }
+        }
+    ];
+
+    it('should return the value 50 from array of null values', () => {
+        let find = arrayFind(arr);
+        assert.equal(50, find('y'));
+    });
+});
+
+describe('sortBy:', () => {
+    const arr = [
+        {
+            funnel_id: 123456,
+            test: true,
+            cf_uvid: '123512351234'
+        },
+        {
+            funnel_id: 54321,
+            test: false,
+            cf_uvid: '123512351234'
+        },
+        {
+            funnel_id: 612361,
+            test: true,
+            cf_uvid: '123512351234'
+        },
+        {
+            funnel_id: 12356,
+            test: true,
+            cf_uvid: '71237218'
+        }
+    ];
+
+    it('should return an array of objects sorted by a specific key', () => {
+        let sort = sortBy(arr);
+        let orders = sort('funnel_id', 'orders');
+        assert.equal(true, orders.hasOwnProperty("123456"));
+    });
+})
+
+describe('arraySet', () => {
+    it('should set a value on the entire array at the selected path', () => {
+        const path = 'a.b.c';
+        let arr = [
+            {
+                a: {
+                    b: {
+                        c: 'BAD'
+                    }
+                }
+            },
+            {
+                a: {
+                    b: {
+                        c: 'BAD'
+                    }
+                }
+            },
+            {
+                a: {
+                    b: {
+                        c: 'BAD'
+                    }
+                }
+            },
+            {
+                a: {
+                    b: {
+                        c: 'BAD'
+                    }
+                }
+            }
+        ];
+        arr = arraySet(arr)(path, 'GOOD')
+        for (let i = 0; i < arr.length; i++) {
+            if (deepFind(arr[i], 'c') !== 'GOOD') {
+                return false;
+            }
+        }
+    });
+});
